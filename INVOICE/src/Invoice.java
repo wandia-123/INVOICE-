48305121cb8d9307e9a0b6e9ab7d60c5a0b0d39c
wandia-123 <moreenewandia@gmail.com>
2025-11-26 05:19:47 -0800

    Add files via upload

---
+++ INVOICE/src/Invoice.java
@@ -0,0 +1,31 @@
+public class Invoice {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("                                    INVOICE                                              ");
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Bill To:                                                                 Ship To:");
+        System.out.println("C026-01-0787/2024                                                         NDUNG'U MOREENE WANDIA");
+        System.out.println("No 23/2, SBI Colony.                                                      No 23/2, SBI Colony.");
+        System.out.println("Ragavendra Nagar, Chennai - 600124                                        Ragavendra Nagar, Chennai - 600124");
+        System.out.println("GSTIN:33APFSDF1ZV                                                         GSTIN:33APFSDF1ZV");
+        System.out.println("Contact: +91-7869825463                                                   Contact: +91-7869825463");
+        System.out.println("Email: purchase-team@thendral.com                                         Email: purchase-team@thendral.com");
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("Payment Date: 7 days from date of delivery             Payment Terms: 100% against invoice");
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("S.No   Product Code   Product Name        HSN code    Quantity   Units   Rate    Tax   Amount");
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("1      105            Surf Excel 5 kg     34019011    20         nos     600.00  5%    12600.00");
+        System.out.println("2      109            Rin 1 kg            34019011    25         nos      85.00  5%     2231.00");
+        System.out.println("3      115            Hamam soap 150 g    34011100    25         nos      60.00  5%     1575.00");
+        System.out.println("4      120            Lux Soap 150 g      34011100    30         nos      53.00  5%      443.00");
+        System.out.println("5      125            Dove soap 125 g     34011100    25         nos      75.00  5%     1968.00");
+        System.out.println("6      129            Vim bar 200 g       08109010    20         nos      15.00  5%      315.00");
+        System.out.println("7      131            Pepsodent 150 g     33061020    30         nos      85.00 18%     3009.00");
+        System.out.println("----------------------------------------------------------------------------------------");
+        System.out.println("                                                           Total               22141.00");
+        System.out.println("                                                           Discounts             141.00");
+        System.out.println("                                                           Grand total         22000.00");
+        System.out.println("----------------------------------------------------------------------------------------");
+    }
+}
